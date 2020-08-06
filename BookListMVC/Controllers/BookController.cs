18bfc8e0ed5e184019ca7d95be5d4b853e01c690
Diedinium <jakethomashall@gmail.com>
2020-08-06 23:29:03 +0100

    Continued work on MVC, added initial migration, started work on index, updated client side libraries via LibMan

---
+++ BookListMVC/Controllers/BookController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListMVC.Controllers
+{
+    public class BookController : Controller
+    {
+        private ApplicationDbContext _db;
+
+        public BookController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IndexModel indexModel = new IndexModel { Books = await _db.Book.ToListAsync() };
+
+            return View(indexModel);
+        }
+    }
+}
